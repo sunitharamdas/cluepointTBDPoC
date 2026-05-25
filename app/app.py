07085c8fa7cb286cd2b12135395c17915fb72e4b
Sunitha Vallabhaneni <suni.ramdas@gmail.com>
2026-05-26 00:42:56 +0100

    fix: use raw byte string to suppress invalid escape sequence warnings

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ class MyHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        self.wfile.write(b'''
+        self.wfile.write(rb'''
          ##         .
    ## ## ##        ==
 ## ## ## ## ##    ===
